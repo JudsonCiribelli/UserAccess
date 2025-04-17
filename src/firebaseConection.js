ea9e62345c236ee4544c0da0309a4bc45837dbf1
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-17 20:09:26 -0300

    feat: add firebase setup

---
+++ src/firebaseConection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZj7fDQ3nHTx6Rhwt0BBj9BuL1YD5-vi0",
+  authDomain: "useraccess-3a491.firebaseapp.com",
+  projectId: "useraccess-3a491",
+  storageBucket: "useraccess-3a491.firebasestorage.app",
+  messagingSenderId: "260965129172",
+  appId: "1:260965129172:web:d510abe01f880b7b8ad441",
+  measurementId: "G-2JEEQBBH26",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export { db };
