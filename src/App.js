9859368f9d62378d9e0ffeff458beee938a7dfd4
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-18 19:12:56 -0300

    feat: add firabase conection  and ensure user data can be save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
-function App() {
+import { useState } from "react";
+import "./App.css";
+import { db } from "./firebaseConection";
+import { addDoc, collection } from "firebase/firestore";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState(0);
+
+  const handleAddUser = async () => {
+    await addDoc(collection(db, "User"), {
+      name: name,
+      email: email,
+      idade: age,
+    })
+      .then(() => {
+        console.log("Dados enviados com sucesso!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setAge("");
+    setEmail("");
+    setName("");
+  };
+
   return (
     <div className="App">
-      <h1>ola</h1>
+      <div className="container">
+        <div className="input-container">
+          <label>Nome</label>
+          <input
+            value={name}
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Email</label>
+          <input
+            value={email}
+            type="email"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Idade</label>
+          <input
+            value={age}
+            type="number"
+            placeholder="Digite sua idade"
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <button type="submit" onClick={handleAddUser}>
+          Cadastrar
+        </button>
+        <div>
+          <h1>Nome: {name}</h1>
+          <h1>Email: {email}</h1>
+          <h1>Idade: {age}</h1>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
