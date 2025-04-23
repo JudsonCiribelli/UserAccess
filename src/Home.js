1a83610d211078a0077ab2e35aa75288c07ce9d8
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-22 21:01:50 -0300

    feat: add initial setup on register user page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { auth, db } from "./firebaseConection";
-import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { auth } from "./firebaseConection";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
@@ -9,9 +8,8 @@ import {
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [age, setAge] = useState("");
+  const [users, setUsers] = useState([]);
   const [userIsLogin, setUserIsLogin] = useState(false);
   const [password, setPassword] = useState("");
 
@@ -30,41 +28,12 @@ const HomePage = () => {
     checkLogin();
   }, []);
 
-  const handleAddUser = async () => {
-    await addDoc(collection(db, "User"), {
-      name: name,
-      email: email,
-      idade: age,
-    })
-      .then(() => {
-        console.log("Dados enviados com sucesso!");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    setAge("");
-    setEmail("");
-    setName("");
-  };
-
-  const searchForUser = async () => {
-    const userRef = doc(db, "User");
-    await getDoc(userRef)
-      .then((snapshot) => {
-        setEmail(snapshot.data().email);
-        setName(snapshot.data().name);
-        setAge(snapshot.data().age);
-        console.log("Usuario encontrado");
-      })
-      .catch((error) => {
-        console.log({ error });
-      });
-  };
-
   const handleLoginUser = async () => {
-    await createUserWithEmailAndPassword(auth, email, password)
+    const user = await createUserWithEmailAndPassword(auth, email, password)
       .then(() => {
         console.log("Usuario cadastrado com sucesso");
+        users.push(user);
+        console.log(user);
       })
       .catch((error) => {
         console.log("Error ao cadastrar usuario");
--- src/register.js
+++ src/register.js
@@ -1,7 +1,58 @@
+import { useState } from "react";
+
 const RegisterUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleAddUser = async () => {
+    alert("Usuario criado com sucesso!");
+  };
   return (
-    <div>
-      <h1>Registre-se</h1>
+    <div className="App">
+      <div className="container">
+        <h1>Registre-se</h1>
+        <div className="input-container">
+          <label>Nome</label>
+          <input
+            value={name}
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Email</label>
+          <input
+            value={email}
+            type="email"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Password</label>
+          <input
+            value={password}
+            type="email"
+            placeholder="Digite sua senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label>Idade</label>
+          <input
+            value={age}
+            type="text"
+            placeholder="Digite sua idade"
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn" onClick={handleAddUser}>
+          Registrar-se
+        </button>
+      </div>
     </div>
   );
 };
