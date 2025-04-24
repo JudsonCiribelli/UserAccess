fde453626786b6a94d6dd7b65b40d5427bd231a6
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-24 11:30:48 -0300

    style: finish header component styless

--- src/Components/Header-Component/headerComponent.js
+++ src/Components/Header-Component/headerComponent.js
@@ -1,19 +1,26 @@
+import { Link } from "react-router-dom";
 import "./headerComponentStyless.css";
 import { FaRegUserCircle } from "react-icons/fa";
 const HeaderComponent = () => {
   return (
     <header>
       <div className="Header-Container">
-        <div>
+        <div className="title">
           <h1>Logo</h1>
         </div>
-        <nav className="navegation">
-          <ul>
-            <li>Services</li>
-            <li>About</li>
-            <FaRegUserCircle />
-          </ul>
-        </nav>
+        <div className="navegation">
+          <button className="btn">
+            <Link className="link">Services</Link>
+          </button>
+          <button>
+            <Link className="link">Register</Link>
+          </button>
+          <button>
+            <Link className="link">
+              <FaRegUserCircle className="icon" />
+            </Link>
+          </button>
+        </div>
       </div>
     </header>
   );
